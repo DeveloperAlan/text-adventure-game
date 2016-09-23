90db8ab10a0b4450941de1d57a76aa3707f28ce4
DeveloperAlan <alanng95@hotmail.com>
2016-09-23 18:39:09 +1000

    rollDice if statement shown to leo

--- example-function-parameters.js
+++ example-function-parameters.js
@@ -1,14 +1,34 @@
-function example1(optional) {
-    if (optional != undefined) {
-        console.log('awesome');
-    } else {
-        example2();
-    }
+var colors = require("colors");
+// function example1(optional) {
+//     if (optional != undefined) {
+//         console.log('awesome');
+//     } else {
+//         example2();
+//     }
+// }
+
+// function example2() {
+//     console.log("This is example 2");
+//     example1("not optional");
+// }
+
+// example1();
+
+var i = true;
+
+function move(introAlready) {
+    if (introAlready == "Haven't intro-ed yet") {
+        console.log("Welcome to town");
+    } 
+    console.log("What do you want to do?".yellow);
+    if (i == true) {
+        i = false;
+        move("I have already been introed");
+    }    
 }
 
-function example2() {
-    console.log("This is example 2");
-    example1("not optional");
-}
+move("Haven't intro-ed yet");
 
-example1();
+if(rollDice() == 1 || rollDice() == 2) {
+    //..
+}
